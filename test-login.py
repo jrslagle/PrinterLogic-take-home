7c64a20e74539b0b49b1e50364aa087d0930be8a
James <jrslagle@gmail.com>
2020-02-09 20:35:09 -0700

    added a test for logging in with the wrong password

--- test-login.py
+++ test-login.py
@@ -6,6 +6,7 @@ Created on Fri Feb  7 23:02:53 2020
 """
 
 import sys
+import time
 from selenium import webdriver # is chromedriver in my path?
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,6 +23,17 @@ def main():
         print("Exception:", sys.exc_info()[1])
         print("[FAIL] Simple login failed.")
         driver.quit()
+        sys.exit(1)
+        
+    try:
+        wrong_password(username, password+"$")
+        print("[PASS] Correct password required.")
+    except:
+#        print("Exception:", sys.exc_info()[1])
+        print("[FAIL] Logged in with incorrect password.")
+    
+    driver.quit()
+
 
 def site_login(username,password):
     driver.get ("https://rubixdesign.printercloud.com/admin/")
@@ -33,11 +45,24 @@ def happy_path(username, password):
     
     site_login(username, password)
 
-    WebDriverWait(driver, 10).until(
+    WebDriverWait(driver, 5).until(
         EC.presence_of_element_located((By.XPATH, find_text(username)))
     ).click()
     
     driver.find_element_by_xpath(find_text("Log Out")).click()
+        
+def wrong_password(username, password):
+    
+    site_login(username, password)
+
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, find_text(username)))
+        )
+    except:
+        return
+        
+    raise BaseException
         
 def find_text(text):
     return "//*[contains(text(), '"+text+"')]"
